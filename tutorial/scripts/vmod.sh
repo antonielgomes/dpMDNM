1246fd0fbabe70255420277e1afba660c375fbf2
Antoniel A. S. Gomes <75452450+antonielgomes@users.noreply.github.com>
2024-06-01 20:07:15 +0200

    Delete tutorial/scripts directory

--- tutorial/scripts/vmod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-nreps=$(wc -l inputs/list-modes.txt | awk '{print $1}')
-
-for i in $(seq 1 $nreps); do
- mkdir -p vmod/mode-${i}
- /usr/local/charmm/c41b1/charmm -i scripts/vmod.inp modnu=${i} -o vmod/mode-${i}/vmod-${i}.out
-done
